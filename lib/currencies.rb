c5040817469db2d11cf5ac9f6c2d434b96a9a050
Jan Riethmayer <jan@riethmayer.de>
2010-11-20 00:22:19 +0100

    refactor Currencies

--- lib/currencies.rb
+++ lib/currencies.rb
@@ -1,24 +1,28 @@
 class Currencies
-  def initialize
-    @@currencies = {}
-  end
+  @@currencies = {}
 
-  def add(currency)
-    currency = currency.to_s.downcase
-    result = @@currencies[currency] = Currency.new(currency)
-    self.class.class_eval do
-      define_method currency do
-        @@currencies[currency]
+  class << self
+    def add(currency)
+      currency = currency.to_s.downcase
+      result = @@currencies[currency] = Currency.new(currency)
+      Currencies.class_eval do
+        define_method currency do
+          @@currencies[currency]
+        end
       end
+      result
     end
-    result
-  end
 
-  def all
-    @@currencies.values
-  end
+    def all
+      @@currencies.values
+    end
 
-  def include?(currency)
-    @@currencies.keys.include?(currency.downcase)
+    def include?(currency)
+      @@currencies.keys.include?(currency.downcase)
+    end
+
+    def delete
+      @@currencies = {}
+    end
   end
 end
--- spec/currencies_spec.rb
+++ spec/currencies_spec.rb
@@ -2,16 +2,24 @@ require File.expand_path(File.dirname(__FILE__) +  '/spec_helper.rb')
 describe Currencies do
   context "Adding currencies" do
     before :each do
-      @c = Currencies.new
+      Currencies.delete
     end
-
     it "should add usd" do
-      @c.add("USD").should be_instance_of(Currency)
+      Currencies.add("USD").should be_instance_of(Currency)
     end
 
     it "should include usd" do
-      @c.add("USD")
-      @c.should include("USD")
+      Currencies.add("USD")
+      Currencies.should include("USD")
+    end
+
+    it "should include usd in #all" do
+      Currencies.add("USD")
+      Currencies.all.map(&:code).should include("USD")
+    end
+
+    it "should have no values initially" do
+      Currencies.all.should == []
     end
   end
 end
