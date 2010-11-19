357b7b608338476d3475cd717416203e86deaa17
Jan Riethmayer <jan@riethmayer.de>
2010-11-19 23:17:55 +0100

    add fetching specs

--- spec/currency_updater_spec.rb
+++ spec/currency_updater_spec.rb
@@ -1,6 +1,44 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
 describe "CurrencyUpdater" do
+
+  describe "Fetching currencies" do
+
+    before :all do
+      @cu = CurrencyUpdater.new
+      @codes = @cu.codes
+    end
+
+    it "should fetch all currencies defined by ISO 4217" do
+      @codes.each do |code|
+        currency = @cu.send code.downcase
+        currency.should_not be_nil
+      end
+    end
+
+    it "should override all default rates" do
+      @codes.each do |code|
+        currency = @cu.send code.downcase
+        currency.rate.should_not == 1.0000
+      end
+    end
+
+    it "should set all three letter codes properly" do
+      @codes.each do |code|
+        currency = @cu.send code.downcase
+        currency.code.should == code
+      end
+    end
+
+    it "should translate all names" do
+      @codes.each do |code|
+        code_name = code.downcase
+        currency = @cu.send code_name
+        currency.name.should == I18n.t("currency.#{code_name}")
+      end
+    end
+  end
+
   before :each do
     @cu = CurrencyUpdater.new
     @cu.currencies = ["USD"]
